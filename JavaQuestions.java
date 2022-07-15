ffcc15855d1abf923a5bed262673be0fed60ba6a
Anuthiya <anuthiya04@gmail.com>
2022-07-15 14:43:23 +0530

    Questions

---
+++ JavaQuestions.java
@@ -0,0 +1,97 @@
+package Questions;
+
+public class JavaQuestions {
+
+
+//*/ 1.	1. How we can create immutable class in java 
+//2.	2. Diff bw Collection and collections
+//3.	3. Diff bw list and map
+//4.	4. Diff bw Arraylist and hashset
+//5.	5. Diff bw list, set, map
+//6.	6. What are the possible ways of iteration in list set and map
+//7.	7. What is the default memory of list , map, set
+//8.	8. How to sort one element in list using stream
+//9.	9. What is hashmap and concurrent hashmap
+//10.	10. List converts sublist using array
+//
+//1.	11. What is JAR file
+//2.	12. What is WAR file
+//3.	13. Sorting using compare to………
+//4.	14. Diff bw compareto and compare
+//5.	15. What is synchronization
+//6.	16. What is connection pooling 
+//7.	17. How to debug the code
+//8.	18. What is the prepared statement
+//9.	19. What are the methods in object class and explain
+//10.	20. What is shallow copy and deepcopy
+//11.	21. What is clonable interface
+//12.	22. Diff bw stream and parallel stream in interface
+//13.	23. Diff bw immutable class and singleton class
+//14.	24. Are you aware of immutable class
+//15.	25. Diff bw == and equals method
+//16.	26. What is serialization
+//17.	27. What is custom exception 
+//18.	28. What is IOException
+//19.	29. Explain try, catch, finally
+//20.	30. Diff bw throw and throws
+//
+//1.	31. What are the features of java 8
+//2.	32. Explain enum
+//3.	33. Explain static keyword
+//4.	34. What is static block
+//5.	35. Diff bw heap memory and pool memory
+//6.	36. What is string
+//7.	37. What are the ways to declare a string
+//8.	38. Why string  is immutable
+//9.	39. How to override to string method
+//10.	40. Split the string using regex
+//
+//1.	41. What is Concurrent hashmap and how its work
+//2.	42. What is hashtable
+//3.	43. Diff bw hashmap and hashTable
+//4.	44. Compare list and set
+//5.	45. Diff bw Hashcode and hashtable
+//6.	46. What is load factor
+//7.	47. Explain hashcollision
+//8.	48. Diff bw map and flatmap
+//9.	49. Use of hashmap
+//10.	50. How to implement serialization with scenario or sent
+//
+//1.	51. Write a code for reverse string
+//2.	52. Find occurance of the character in the given string
+//3.	53. Explain OOPs concepts
+//4.	54. Where we use interface concepts in inheritance
+//5.	55. What is the problem occurred in multiple inheritance
+//6.	56. What is super keyword
+//7.	57. Explain this keyword in java
+//8.	58. Difference bw overloading and overriding
+//9.	59. What is encapsulation
+//10.	60. What is data abstraction, how we achieve that.
+//
+//1.	61. Diff bw interface and abstraction
+//2.	62. Can we have implemented methods in interface 
+//3.	63. What is interface
+//4.	64. What is functional interface
+//5.	65. What is marker interface
+//6.	66. What is abstract class
+//7.	67. Explain final and finalize
+//8.	68. What is immutable class
+//9.	69. How we can create an immutable class in java , using final keyword
+//10.	70. What is singleton class
+//
+// 
+//1.	71. What is Sychronization and non synchronization
+//2.	72. What is lambda expression
+//3.	73. Diff bw linkedlist and vector
+//4.	74. Write the code for lambda expression
+//5.	75. All types of conversion in collection
+//6.	76. Explain hashing technique
+//7.	77. Diff bw poll() and remove()
+//8.	78. Hashmap and hashset which is faster
+//9.	79. Use of stream
+//10.	80. How to traverse the linked list
+//11.	81. How to traverse the normal arraylist 
+//
+//
+///*
+}
---
+++ SpringBootQuestions.java
@@ -0,0 +1,115 @@
+package Questions;
+
+public class SpringBootQuestions {
+//	1.	1. Query param/Pathvariable/RequestParam
+//	2.	2. Http methods
+//	3.	3. What is RequestingMapping
+//	4.	4. What is dependency injection
+//	5.	5. Use of Pom.xml
+//	6.	6. Difference between Microservice and monolithic
+//	7.	7. Difference between RestController and Controller
+//	8.	8. What are the basic annotation in springboot
+//	9.	9. What is hibernate
+//	10.	10. How to connect database using url and where
+//	11.	11. Springboot flow
+//	12.	12. Application properties? What are the benefits of using Application properties?
+//	13.	13. Why we are using strarter project?
+//	14.	14. What is the difference b/w spring project and spring boot project
+//	15.	15. What is Auto configuration
+//	16.	16. What is JPA
+//	17.	17. Delete truncate and drop which can be rollback
+//	18.	18. What is RestAPI
+//
+//	1.	19. Types of Annotations
+//	(i). @Request Mapping
+//	(ii). @Controller
+//	(iii). @Autowired
+//	(iv). @Repository
+//	(v). @Service
+//	(vi). @Pathvariable
+//	(vii). @RequestParam
+//	(viii). @ResponseBody
+//	(ix). @ResponseEntity
+//	(x). Spring boot Application
+//	(xi). @Bean
+//	2.	20. Dispatch servelet
+//	3.	21. Handler mapping
+//	4.	22. Difference between @RestController and @Controller
+//	5.	23. DB connnection
+//	6.	24. What is springboot
+//	7.	25. Write a code to fetch the values from database in springboot
+//	8.	26. How to create a project in a spring boot by step by step 
+//	9.	27. Where will you  check it UI server is not append
+//
+//	1.	28. How will you check the navigation error in service and UI
+//	2.	29. Write a code for postmethod in springboot
+//	3.	30. Explain spring
+//	4.	31. What is the application server with technical explanation
+//	5.	32. What are the annotations in spring
+//	6.	33. Explain about spring mvc architecture
+//	7.	34. What are the annotations used in your project and explain it
+//	8.	35. What is dependency injection
+//	9.	36. How to inject jdbc
+//	10.	37. What is pom.xml
+//
+//	1.	38. What is tomcat server
+//	2.	39. Evoluation of spring
+//	3.	40. What is JSON
+//	4.	41. How to use postman and its uses
+//	5.	42. What is a diff bw get/Post/delete/put
+//	6.	43. How to add details in application properties, how to create application properties
+//	7.	44. Diff bw postmapping and requestmapping 
+//	8.	45. Explain queryparam
+//	9.	46. Diff bw maven and gradle
+//	10.	47. What is http errors
+//	11.	48. What is @Springboot application
+//	12.	49. Which class is used to communicate database
+//
+//	1.	50. How can you change default server
+//	2.	51. Diff bw microservices and monolithic
+//	3.	52. Diff bw Restcontroller and controller
+//	4.	53. How can you change default server
+//	5.	54. How can you change catch in hibernate
+//	6.	55. How to connect DB using url and where we connect
+//	7.	56. What is auto configuration
+//	8.	57. Have you worked in profile
+//
+//	1.	58. Are you know design patterns
+//	2.	59. What is global exception and how to handle global exception
+//	3.	60. Diff bw spring and springboot
+//	4.	61. What are the dependcies incluse in standalone applications
+//	5.	62. What is RestAPI
+//	6.	63. Have you create an any restAPI
+//	7.	64. How you created any endpoint in your restAPI
+//	8.	65. What is the difference bw delete/truncate/drop, Which can be roll back among these 3.
+//	9.	66. SDLC process
+//	10.	67. Rest Vs MicroServices
+//	11.	68. Spring core vs springboot vs spring mvc in spring security
+//	12.	69. Java configuration
+//
+//	1.	70. How to create springboot project
+//	2.	71. Agile methodology
+//	3.	72. What is swagger why we are use swagger plugin
+//	4.	73. How digital certificates used in microservices
+//	5.	74. Testing junit
+//	6.	75. What is authentication session
+//	7.	76. What is eureka, zuul, and Ribbon
+//	8.	77. What is docker container
+//	9.	78. CI/CD/CT contains integration
+//	10.	79. Cors policy
+//
+//	1.	80. What is spring scheduler
+//	2.	81. What is spring jdbc
+//	3.	82. How to enable spring security in STS
+//	4.	83. Exception handler in STS
+//	5.	84. What is Log4j
+//	6.	85. Explain about ORM tools
+//	7.	86. Where will ur struggle in technical part in project and how will u resolved it
+//	8.	87. Explain version controller
+//	9.	88. Tell me about your project
+//	10.	89. Explain about your module
+//	11.	90. Role in your module
+//	12.	91. Have u done version controller in your project
+//	13.	92. Tell me about 10 error in ur project
+//	14.	93. Explain version controller 
+}
